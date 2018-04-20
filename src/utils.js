cabe07bde29f1b0d9bb531fe439eb32adaea5511
Ivan Shmidt <bahoc47@gmail.com>
2018-04-20 12:50:12 +0300

    - exports

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,18 @@
 const WAVES_ID_TYPE = null;
 
-export const compareUint8Arrays = (arr1, arr2) => {
+const compareUint8Arrays = (arr1, arr2) => {
   //  true    - arr1 bigger
   //  false    - arr2 bigger
   return arr1.toString('hex') > arr2.toString('hex');
 };
-export const isPair = o =>
+const isPair = o =>
   Array.isArray(o) &&
   o.length === 2 &&
   o.every(id => typeof id === 'string' || id === WAVES_ID_TYPE);
-export const isEmptyArray = a => Array.isArray(a) && a.length === 0;
+const isEmptyArray = a => Array.isArray(a) && a.length === 0;
+
+module.exports = {
+  compareUint8Arrays,
+  isPair,
+  isEmptyArray,
+};
