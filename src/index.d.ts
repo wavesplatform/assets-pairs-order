cd6a9bf923789751d1aacf91b73a63296abeea53
Ivan Shmidt <bahoc47@gmail.com>
2018-04-20 17:09:38 +0300

    Fixed types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,15 @@
 // Type definitions for assets-pair-order
 // Definitions by: ishmidt
 declare module '@waves/assets-pairs-order' {
-  export type TOrderPair = (a: string, b: string) => string[];
+  type TPair = [string, string];
+  export type TOrderPair = (a: string, b: string) => TPair;
 
-  export function createOrderPair(predefinedList: string[]): orderPair;
+  export function createOrderPair(predefinedList: string[]): TOrderPair;
   export function createOrderPair(
     predefinedList: string[],
     a: string,
     b: string
-  ): string[];
+  ): TPair;
 
   export const MAINNET_DATA: string[];
 }
