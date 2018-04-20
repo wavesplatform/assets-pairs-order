b03622d51d29b8ce02c485bc6d7c3b0850d9b23f
Ivan Shmidt <bahoc47@gmail.com>
2018-04-20 16:46:09 +0300

    Types defs

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,14 @@
-// Type definitions for assets-pair-order 
+// Type definitions for assets-pair-order
 // Definitions by: ishmidt
-export declare function createOrderPair (predefinedList: string[]) : (a: string, b: string) => string[];
-export declare function createOrderPair(predefinedList: string[], a: string, b: string): string[];
+declare module '@waves/assets-pairs-order' {
+  export type TOrderPair = (a: string, b: string) => string[];
 
-declare const MAINNET_DATA: string[];
+  export function createOrderPair(predefinedList: string[]): orderPair;
+  export function createOrderPair(
+    predefinedList: string[],
+    a: string,
+    b: string
+  ): string[];
+
+  export const MAINNET_DATA: string[];
+}
